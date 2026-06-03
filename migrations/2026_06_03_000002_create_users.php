31413e2b1be2a57ea3973f5b5a5e278dd80750d7
Ihor <igor.yarmolyuk00@gmail.com>
2026-06-03 14:12:08 +0300

    New migration for users database

---
+++ migrations/2026_06_03_000002_create_users.php
@@ -0,0 +1,15 @@
+<?php
+migrate("create table users
+(
+    id         int auto_increment
+        primary key,
+    first_name varchar(255) not null,
+    last_name  varchar(255) not null,
+    username   varchar(255) not null,
+    password   int          not null,
+    email      varchar(255) not null,
+    created_at int          not null,
+    activity   tinyint(1)   not null,
+    constraint users_pk
+        unique (username)
+);");
