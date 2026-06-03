3d3668c39db210a4d09b230fdcb21d302cf447f2
Ihor <igor.yarmolyuk00@gmail.com>
2026-06-03 14:27:56 +0300

    Now the migration task_statuses_table is also fills itself

--- migrations/2026_06_03_000000_create_task_statuses_table.php
+++ migrations/2026_06_03_000000_create_task_statuses_table.php
@@ -5,3 +5,9 @@ migrate("create table task_statuses
         primary key,
     name varchar(255) not null
 );");
+migrate(
+    "INSERT INTO task_statuses (id, name) VALUES (1, 'TODO');" .
+        "INSERT INTO task_statuses (id, name) VALUES (2, 'In progress');" .
+        "INSERT INTO task_statuses (id, name) VALUES (3, 'Completed');" .
+        "INSERT INTO task_statuses (id, name) VALUES (4, 'On halt');"
+);
