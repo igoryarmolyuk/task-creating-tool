40642961baea8d8fa487cbef318b5915b452aeb1
Ihor <igor.yarmolyuk00@gmail.com>
2026-06-03 15:37:41 +0300

    Minor changes

--- migrations/2026_06_03_000002_create_users.php
+++ migrations/2026_06_03_000002_create_users.php
@@ -6,8 +6,8 @@ migrate("create table users
     first_name varchar(255) not null,
     last_name  varchar(255) not null,
     username   varchar(255) not null,
-    password   int          not null,
     email      varchar(255) not null,
+    password   int          not null,
     created_at int          not null,
     activity   tinyint(1)   not null,
     constraint users_pk
